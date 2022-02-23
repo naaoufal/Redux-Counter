7533cb39304bac8704377779ad260cfef40cc4cd
benmansour <naoufelbenmensour@gmail.com>
2022-02-23 18:32:32 +0100

    add files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import Counter from './components/Counter'
-import counterReducer from './reducers/counterReducer'
-
-const store = createStore(counterReducer)
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrement,
+  increment,
+  reset,
+  logIn,
+  logOut,
+} from "./Actions/index";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
   return (
-  <Provider store={store}>
-    <Counter />
-  </Provider>
+    <div className="App">
+      <h1>
+         Hello World <br /> A little Redux Project. YaaY!
+      </h1>
+      <h3>Counter</h3>
+      <h3>{counter}</h3>
+      <button onClick={() => dispatch(increment())}>Increase</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
+      <button onClick={() => dispatch(decrement())}>Decrease</button>
+
+      <h2>For Logged in users only</h2>
+      <p>Log in to see a secret about me</p>
+      <button onClick={() => dispatch(logIn())}>Login</button>
+      <button onClick={() => dispatch(logOut())}>Logout</button>
+      {auth ? (
+        <div>
+          <p>
+            I don't more than 50% of redux. But if you know 50% of it, you're like a Superman.
+          </p>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
   );
 }
 
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import counter from "./counter";
+// import auth from "./auth";
+import { combineReducers } from "redux";
+
+const allReducers = combineReducers({
+  counter,
+//   auth,
+});
+export default allReducers;
